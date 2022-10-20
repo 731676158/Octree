62a0bf30f025dfe47282dc7bb1df49ad157e15b0
731676158 <731676158@qq.com>
2022-10-20 16:40:15 +0800

    add file to transform kitti bin to pcd file; modified boost library.

--- Octree/kitti_bin_to_pcd.cpp
+++ Octree/kitti_bin_to_pcd.cpp
@@ -1,39 +1,90 @@
+#include <boost/program_options.hpp>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/point_operators.h>
+#include <pcl/common/io.h>
+#include <pcl/search/organized.h>
+#include <pcl/search/octree.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/filters/conditional_removal.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/surface/gp3.h>
+#include <pcl/io/vtk_io.h>
+#include <pcl/filters/voxel_grid.h>
+
 #include <iostream>
+#include <fstream>
+
+using namespace pcl;
 using namespace std;
 
-int main()
-{
-	int32_t num = 1000000;
-	float* data = (float*)malloc(num * sizeof(float));
-	// 点
-	float* px = data + 0;
-	float* py = data + 1;
-	float* pz = data + 2;
-	float* pr = data + 3;//反射强度
-	// 读取点云数据
-	FILE* stream;
-	fopen_s(&stream, "data\\0000000000.bin", "rb");
-	num = fread(data, sizeof(float), num, stream) / 4;//读入点云数据，大概10万+个点
-	fclose(stream);
-	/**转换成PCD**/
-	//写文件声明
-	FILE* writePCDStream;
-	fopen_s(&writePCDStream, "0000000000.pcd", "wb");
-	fprintf(writePCDStream, "VERSION 0.7\n");//版本说明
-	fprintf(writePCDStream, "FIELDS x y z\n");//维度说明
-	fprintf(writePCDStream, "SIZE 4 4 4\n");//占用字节说明
-	fprintf(writePCDStream, "TYPE F F F\n");//具体数据类型定义
-	fprintf(writePCDStream, "WIDTH %d\n", num);//点数量
-	fprintf(writePCDStream, "HEIGHT 1\n");//无序点云默认为1
-	fprintf(writePCDStream, "POINTS %d\n", num);//点数量
-	fprintf(writePCDStream, "DATA ascii\n");//文档使用字符类型shuom
-	//写点云数据
-	for (int32_t i = 0; i < num; i++)
-	{
-		fprintf(writePCDStream, "%f %f %f\n", *px, *py, *pz);
-		px += 4; py += 4; pz += 4; pr += 4;
-	}
-	fclose(writePCDStream);
+namespace po = boost::program_options;
 
-	return 0;
+int main(int argc, char** argv) {
+	/////The file to read from.
+	//string infile;
+
+	/////The file to output to.
+	//string outfile;
+
+	//// Declare the supported options.
+	//po::options_description desc("Program options");
+	//desc.add_options()
+	//	//Options
+	//	("infile", po::value<string>(&infile)->required(), "the file to read a point cloud from")
+	//	("outfile", po::value<string>(&outfile)->required(), "the file to write the DoN point cloud & normals to")
+	//	;
+	//// Parse the command line
+	//po::variables_map vm;
+	//po::store(po::parse_command_line(argc, argv, desc), vm);
+
+	//// Print help
+	//if (vm.count("help"))
+	//{
+	//	cout << desc << "\n";
+	//	return false;
+	//}
+
+	//// Process options.
+	//po::notify(vm);
+
+	string infile{ "C:\\files\\point_cloud\\codes\\prt\\lecturePrt\\TreesAndKnn\\Octree\\data\\0000000000.bin" };
+	string outfile{ "C:\\files\\point_cloud\\codes\\prt\\lecturePrt\\TreesAndKnn\\Octree\\data\\0000000000.pcd" };
+
+	// load point cloud
+	fstream input(infile.c_str(), ios::in | ios::binary);
+	if (!input.good()) {
+		cerr << "Could not read file: " << infile << endl;
+		exit(EXIT_FAILURE);
+	}
+	input.seekg(0, ios::beg);
+
+	pcl::PointCloud<PointXYZI>::Ptr points(new pcl::PointCloud<PointXYZI>);
+	//pcl::PointCloud<PointCloud2>::Ptr points (new pcl::PointCloud<PointCloud2>);
+
+	int i;
+	for (i = 0; input.good() && !input.eof(); i++) {
+		PointXYZI point;
+		input.read((char*)&point.x, 3 * sizeof(float));
+		input.read((char*)&point.intensity, sizeof(float));
+		points->push_back(point);
+	}
+	input.close();
+
+	cout << "Read KTTI point cloud with " << i << " points, writing to " << outfile << endl;
+
+	pcl::PCDWriter writer;
+
+	// Save DoN features
+			//pcl::io::savePCDFileBinary(outfile, *points);
+	writer.write<PointXYZI>(outfile, *points, false);
+	//writer.write<PointCloud2> (outfile, *points, false);
+
+	cout << "I CAN SEE YOU" << endl;
+
+
 }
+
+
